a38dff81003aca191c1f2b6dd0032d4618892d03
shashankcheema <77758065+shashankcheema@users.noreply.github.com>
2023-08-01 17:06:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from flask_app_wrapper import FlaskAppWrapper
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from python_code_converter import PythonCodeConverter
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert_code():
+    data = request.get_json()
+    converter = PythonCodeConverter(data['code'], data['version'])
+    converted_code = converter.convert_code()
+    return jsonify({'converted_code': converted_code})
 
 if __name__ == '__main__':
-    app = FlaskAppWrapper()
-    app.run()
+    app.run(debug=True)
