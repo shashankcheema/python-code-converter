d1f9b3883570902637df4fb1b1b63448fdb5aa76
shashankcheema <77758065+shashankcheema@users.noreply.github.com>
2023-08-01 18:00:16 -0400

    Update python_code_converter.py

--- python_code_converter.py
+++ python_code_converter.py
@@ -1,19 +1,23 @@
+import ast
 from lib2to3.refactor import RefactoringTool, get_fixers_from_package
+from lib2to3.pgen2.parse import ParseError
 
 class PythonCodeConverter:
     def __init__(self, code, target_version):
-        self.code = code
+        self.code = '#!/usr/bin/env python\n' + ast.literal_eval(code).strip()
         self.target_version = target_version
 
     def convert_code(self):
         if self.target_version.startswith('3'):
             return self.convert_to_python3()
         else:
-            # For now, we'll just return the original code
             return self.code
 
     def convert_to_python3(self):
         fixers = get_fixers_from_package('lib2to3.fixes')
         tool = RefactoringTool(fixers)
-        tree = tool.refactor_string(self.code, "<string>")
-        return str(tree)
+        try:
+            tree = tool.refactor_string(self.code, "<string>")
+            return str(tree)
+        except ParseError:
+            return "Syntax error in the input code."
